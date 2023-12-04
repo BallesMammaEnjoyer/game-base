35773abbfa99658453c5eb62d56d7cf5857f67ab
Mille Tedebring Thysell <milletedebring.thysell@elev.ga.ntig.se>
2023-12-04 13:18:42 +0100

    gjorde så att sprite och run funkar

--- src/Player.js
+++ src/Player.js
@@ -3,14 +3,14 @@ import Sprites from "./assets/css/Sprites.png"
 export default class Player {
     constructor(game) {
         this.game = game
-        this.width = 20
-        this.height = 30
+        this.width = 32
+        this.height = 32
         this.x = 50
         this.y = 100
 
         this.speedX = 0
         this.speedY = 0
-        this.maxSpeed = 10
+        this.maxSpeed = 4
         this.jumpSpeed = 16
         this.jumpTimer = 0
         this.jumpInterval = 600
@@ -25,9 +25,9 @@ export default class Player {
         this.image = image
 
         //sprite animation
-        this.frameX = 0.3
-        this.frameY = 1
-        this.maxFrame = 8
+        this.frameX = 0
+        this.frameY = 0
+        this.maxFrame = 4
         this.fps = 20
         this.timer = 0
         this.interval = 1000 / this.fps
@@ -38,7 +38,7 @@ export default class Player {
         this.animationTimer = 0
         this.animationInterval = 1000 / this.animationFps
         this.idleFrames = 10
-        this.runFrames = 8
+        this.runFrames = 4
 
 
     }
@@ -81,14 +81,27 @@ export default class Player {
         this.projectiles = this.projectiles.filter(
             (projectile) => !projectile.markedForDeletion
         )
-        
+
         // play run or idle animation
         if (this.speedX !== 0) {
-            this.frameY = 1
+            this.frameY = 0
             this.maxFrame = this.runFrames
         } else {
             this.frameY = 0
-            this.maxFrame = this.idleFrames
+            this.maxFrame = 1
+        }
+
+        // sprite animation update
+        if (this.animationTimer > this.animationInterval) {
+            this.frameX++
+            this.animationTimer = 0
+        } else {
+            this.animationTimer += deltaTime
+        }
+
+        // reset frameX when it reaches maxFrame
+        if (this.frameX >= this.maxFrame) {
+            this.frameX = 0
         }
 
     }
@@ -101,7 +114,7 @@ export default class Player {
         }
     }
     draw(context) {
-        
+
         this.projectiles.forEach((projectile) => {
             projectile.draw(context)
         })
@@ -116,11 +129,11 @@ export default class Player {
             context.save()
             context.scale(-1, 1)
         }
-
+        // sx sy sw sh dx dy dw dh
         context.drawImage(
             this.image,
             this.frameX * this.width,
-            this.frameY * this.height - 14,
+            this.frameY * this.height,
             this.width,
             this.height,
             this.flip ? this.x * -1 - this.width : this.x,
@@ -128,9 +141,11 @@ export default class Player {
             this.width,
             this.height
         )
+        if (this.flip) {
 
-        context.restore()
-    } 
+            context.restore()
+        }
+    }
 
     shoot() {
         if (this.shootTimer > this.shootDelay) {
